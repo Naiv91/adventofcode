81a048358ad6d07423bb18378849c0193dc1435e
Kristian Norum Karlsen <kristian@ad.eyeshare.no>
2024-12-11 23:50:50 +0100

    day 11

---
+++ 11/Part1.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections.Generic;
+using System.Data;
+using System.Runtime.InteropServices;
+using System.Threading;
+
+public class Part1(List<string> input)
+{
+    public void Solve()
+    {
+        var stones = input.First().Split(' ').Select(c => long.Parse(c)).ToList();
+
+        for (int i = 0; i < 25; i++)
+        {
+            List<long> temp = [];
+
+            for (int j = 0; j < stones.Count; j++)
+            {
+                var stone = stones[j];
+                var strStone = stone.ToString();
+
+                if (stone == 0)
+                    temp.Add(1);
+                else if (strStone.Length % 2 == 0)
+                {
+                    temp.Add(long.Parse(strStone.Substring(0, (strStone.Length / 2))));
+                    temp.Add(long.Parse(strStone.Substring(((strStone.Length / 2)), (strStone.Length / 2))));
+                }
+                else
+                {
+                    temp.Add(stone * 2024);
+                }
+            }
+
+            stones = [.. temp];
+        }
+
+        Console.WriteLine(stones.Count);
+    }
+}
---
+++ 11/Part2.cs
@@ -0,0 +1,32 @@
+﻿using System.Text.RegularExpressions;
+public class Part2(List<string> input)
+{
+    public void Solve()
+    {
+        var stones = input.First().Split(' ').Select(c => long.Parse(c)).ToList();
+
+        Dictionary<(long stone, int blink), long> Knowns = [];
+
+        Console.WriteLine(stones.Sum(s => CheckStone(s, 75)));
+
+        long CheckStone(long stone, int blink)
+        {
+            if (Knowns.TryGetValue((stone, blink), out long res))
+                return res;
+
+            var strStone = stone.ToString();
+
+            if (blink == 0)
+                res = 1;
+            else if (stone == 0)
+                res = CheckStone(1, blink - 1);
+            else if (strStone.Length % 2 == 0)
+                res = CheckStone(long.Parse(strStone.Substring(0, (strStone.Length / 2))), blink - 1) + CheckStone(long.Parse(strStone.Substring(((strStone.Length / 2)), (strStone.Length / 2))), blink - 1);
+            else
+                res = CheckStone(stone * 2024, blink - 1);
+
+            Knowns.Add((stone, blink), res);
+            return res;
+        }
+    }
+}
