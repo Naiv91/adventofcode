cc4cacecb63d8a8223afa669679d1bfac50877e1
Kristian Norum Karlsen <kristian@ad.eyeshare.no>
2024-12-12 00:03:24 +0100

    nice nice

--- 11/Part2.cs
+++ 11/Part2.cs
@@ -5,13 +5,13 @@ public class Part2(List<string> input)
     {
         var stones = input.First().Split(' ').Select(c => long.Parse(c)).ToList();
 
-        Dictionary<(long stone, int blink), long> Knowns = [];
+        Dictionary<(long stone, int blink), long> knowns = [];
 
         Console.WriteLine(stones.Sum(s => CheckStone(s, 75)));
 
         long CheckStone(long stone, int blink)
         {
-            if (Knowns.TryGetValue((stone, blink), out long res))
+            if (knowns.TryGetValue((stone, blink), out long res))
                 return res;
 
             var strStone = stone.ToString();
@@ -21,11 +21,11 @@ public class Part2(List<string> input)
             else if (stone == 0)
                 res = CheckStone(1, blink - 1);
             else if (strStone.Length % 2 == 0)
-                res = CheckStone(long.Parse(strStone.Substring(0, (strStone.Length / 2))), blink - 1) + CheckStone(long.Parse(strStone.Substring(((strStone.Length / 2)), (strStone.Length / 2))), blink - 1);
+                res = CheckStone(long.Parse(strStone[..(strStone.Length / 2)]), blink - 1) + CheckStone(long.Parse(strStone[(strStone.Length / 2)..]), blink - 1);
             else
                 res = CheckStone(stone * 2024, blink - 1);
 
-            Knowns.Add((stone, blink), res);
+            knowns.Add((stone, blink), res);
             return res;
         }
     }
