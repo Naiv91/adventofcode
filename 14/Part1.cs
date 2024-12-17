efe20705c11ca0d0674e15adf071369a5a27485e
Kristian Norum Karlsen <kristian@ad.eyeshare.no>
2024-12-17 01:29:27 +0100

    day 14

---
+++ 14/Part1.cs
@@ -0,0 +1,84 @@
+﻿public class Part1(List<string> input)
+{
+    public void Solve()
+    {
+        var robots = input.Select(x => x.Split(' ')).Select(parts => (p: (x: int.Parse(parts[0].Split('=')[1].Split(',')[0]), y: int.Parse(parts[0].Split('=')[1].Split(',')[1])), v: (x: int.Parse(parts[1].Split('=')[1].Split(',')[0]), y: int.Parse(parts[1].Split('=')[1].Split(',')[1])))).ToList();
+        int maxX = 100;
+        int maxY = 102;
+        int mX = (maxX + 1) / 2;
+        int mY = (maxY + 1) / 2;
+
+        var finalPos = robots.Select(x => FinalPosition(x.p, x.v, 100, [])).ToList();
+        var finalBots = finalPos.Where(r => r.x != mX && r.y != mY).ToList();
+
+        var quads = (0, 0, 0, 0);
+
+        foreach (var robot in finalBots)
+        {
+            if (robot.x < mX && robot.y < mY)
+                quads.Item1++;
+            else if (robot.x > mX && robot.y < mY)
+                quads.Item2++;
+            else if (robot.x < mX && robot.y > mY)
+                quads.Item3++;
+            else
+                quads.Item4++;
+
+        }
+
+        Console.WriteLine(quads.Item1 * quads.Item2 * quads.Item3 * quads.Item4);
+
+        (int x, int y) FinalPosition((int x, int y) p, (int x, int y) v, int tick, List<(int x, int y)> visited)
+        {
+            if (tick < 0)
+                return visited.Last();
+
+            visited.Add(p);
+            var next = CalcNewPos(p, v);
+            return FinalPosition(next, v, tick - 1, visited);
+        }
+
+        (int x, int y) CalcNewPos((int x, int y) p, (int x, int y) v)
+        {
+            if (int.IsPositive(v.x))
+            {
+                if ((p.x + v.x) > maxX)
+                    p.x = ((p.x + v.x) - maxX) - 1;
+                else
+                {
+                    p.x += v.x;
+                }
+
+            }
+            else
+            {
+                if ((p.x + v.x) < 0)
+                    p.x = (maxX - Math.Abs((p.x + v.x))) + 1;
+                else
+                {
+                    p.x += v.x;
+                }
+            }
+
+            if (int.IsPositive(v.y))
+            {
+                if ((p.y + v.y) > maxY)
+                    p.y = ((p.y + v.y) - maxY) - 1;
+                else
+                {
+                    p.y += v.y;
+                }
+            }
+            else
+            {
+                if ((p.y + v.y) < 0)
+                    p.y = (maxY - Math.Abs((p.y + v.y))) + 1;
+                else
+                {
+                    p.y += v.y;
+                }
+            }
+            return p;
+        }
+    }
+}
---
+++ 14/Part2.cs
@@ -0,0 +1,108 @@
+﻿public class Part2(List<string> input)
+{
+    class Robot
+    {
+        public (int x, int y) P;
+        public (int x, int y) V;
+    }
+
+    private readonly (int width, int height) Grid = (100, 102);
+
+    public void Solve()
+    {
+        var robots = input.Select(x => x.Split(' ')).Select(parts => new Robot { P = (x: int.Parse(parts[0].Split('=')[1].Split(',')[0]), y: int.Parse(parts[0].Split('=')[1].Split(',')[1])), V = (x: int.Parse(parts[1].Split('=')[1].Split(',')[0]), y: int.Parse(parts[1].Split('=')[1].Split(',')[1])) }).ToList();
+        string filePath = "output.txt"; // Define the file path
+        using var writer = new StreamWriter(filePath);
+
+        int ticks = 0;
+        while (true)
+        {
+            robots.ForEach(robot =>
+            {
+                robot.P = CalcNewPos(robot.P, robot.V);
+
+            });
+
+            var hasConsecutive = robots.GroupBy(robot => robot.P.x).Select(r => r.Select(r => r.P).OrderBy(p => p.y).ToList()).Where(r => r.Count > 10).ToList().Any(group =>
+            {
+                int consec = 0;
+
+                for (int i = 0; i < group.Count - 1; i++)
+                {
+                    consec = (group[i].y + 1) == group[i + 1].y ? consec + 1 : 0;
+
+                    if (consec >= 10)
+                        return true;
+                }
+
+                return false;
+            });
+
+            if (hasConsecutive)
+            {
+                var grid = Enumerable.Range(0, 103).Select(y => Enumerable.Range(0, 101).Select(x => '.').ToList()).ToList();
+                robots.ForEach(robot =>
+                {
+                    grid[robot.P.y][robot.P.x] = 'X';
+
+                });
+
+                Console.WriteLine($"\n{new string('#', 43)} Line {ticks} {new string('#', 43)}\n");
+                Console.WriteLine(string.Join(string.Empty, grid.SelectMany(line => string.Join(string.Empty, line) + "\n")) + "\n");
+
+                writer.WriteLine($"\n{new string('#', 43)} Line {ticks} {new string('#', 43)}\n");
+                writer.WriteLine(string.Join(string.Empty, grid.SelectMany(line => string.Join(string.Empty, line) + "\n")) + "\n");
+            }
+
+            ticks++;
+        }
+
+        (int x, int y) CalcNewPos((int x, int y) p, (int x, int y) v)
+        {
+            if (int.IsPositive(v.x))
+            {
+                if ((p.x + v.x) > Grid.width)
+                    p.x = ((p.x + v.x) - Grid.width) - 1;
+                else
+                {
+                    p.x += v.x;
+                }
+
+            }
+            else
+            {
+                if ((p.x + v.x) < 0)
+                    p.x = (Grid.width - Math.Abs((p.x + v.x))) + 1;
+                else
+                {
+                    p.x += v.x;
+                }
+            }
+
+            if (int.IsPositive(v.y))
+            {
+                if ((p.y + v.y) > Grid.height)
+                    p.y = ((p.y + v.y) - Grid.height) - 1;
+                else
+                {
+                    p.y += v.y;
+                }
+            }
+            else
+            {
+                if ((p.y + v.y) < 0)
+                    p.y = (Grid.height - Math.Abs((p.y + v.y))) + 1;
+                else
+                {
+                    p.y += v.y;
+                }
+            }
+
+            return p;
+        }
+
+    }
+}
+
+
+
