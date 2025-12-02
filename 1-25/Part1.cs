193b04fcb5df3c5187d16dfb12baedd69c5e8136
Kristian Norum Karlsen <kristian@ad.eyeshare.no>
2025-12-03 00:59:09 +0100

    fix

---
+++ 1-25/Part1.cs
@@ -0,0 +1,47 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace _1;
+
+public class Part1(List<string> input)
+{
+
+    public void Solve()
+    {
+        int dial = 50;
+        int leftAtZero = 0;
+        int everyZeroTick = 0;
+        foreach (var line in input)
+        {
+            var dir = line[0];
+            var rot = int.Parse(line[1..]);
+
+            for (var i = 0; i < rot; i++)
+            {
+                if (dir == 'L')
+                {
+                    if (dial == 0)
+                        dial = 99;
+                    else
+                        dial--;
+                }
+                else
+                {
+                    if (dial == 100)
+                        dial = 1;
+                    else
+                        dial++;
+                }
+
+                if (dial == 0 || dial == 100)
+                {
+                    everyZeroTick++;
+                }
+            }
+
+            if (dial == 0 || dial == 100)
+                leftAtZero++;
+        }
+        Console.WriteLine($"Part1: {leftAtZero}");
+        Console.WriteLine($"Part2: {everyZeroTick}");
+    }
+}
---
+++ 1-25/Program.cs
@@ -0,0 +1,7 @@
+﻿using _1;
+using _2;
+using System.IO;
+
+var text = File.ReadAllLines(Directory.GetCurrentDirectory() + "\\input.txt").ToList();
+new Part1(text).Solve();
+// new Part2(text).Solve();
