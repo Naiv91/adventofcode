3249ea95d8c089f3fa5c64403b7c9e2ddaad9b21
Kristian Norum Karlsen <kristian@ad.eyeshare.no>
2025-12-07 00:20:11 +0100

    fix

---
+++ 2025/5/Part1.cs
@@ -0,0 +1,28 @@
+﻿using System.Diagnostics;
+using System.Runtime.InteropServices;
+
+namespace _1;
+
+public partial class Part1(List<string> input)
+{
+    public void Solve()
+    {
+        var ranges = input.TakeWhile(line => !string.IsNullOrWhiteSpace(line)).Select(line => line.Split('-').Select(x => long.Parse(x)).ToList()).ToList();
+        var ids = input.Skip(ranges.Count + 1).Select(i => long.Parse(i)).ToList();
+        int fresh = 0;
+
+        foreach (var id in ids)
+        {
+            foreach (var range in ranges)
+            {
+                if (id >= range[0] && id <= range[1])
+                {
+                    fresh++;
+                    break;
+                }
+            }
+        }
+
+        Console.WriteLine(fresh);
+    }
+}
---
+++ 2025/5/Part2.cs
@@ -0,0 +1,34 @@
+﻿namespace _2;
+
+public partial class Part2(List<string> input)
+{
+    public void Solve()
+    {
+        var ranges = input.TakeWhile(line => !string.IsNullOrWhiteSpace(line)).Select(line => line.Split('-').Select(x => long.Parse(x)).ToList()).OrderBy(r => r[0]).ToList();
+        var starts = new List<long>();
+        var ends = new List<long>();
+
+        CleanRanges();
+        void CleanRanges()
+        {
+            foreach (var test in ranges.OrderBy(r => r[0]))
+            {
+                var toCheck = ranges.Where(r => r != test && test[1] >= r[0] && test[1] <= r[1]).OrderBy(r => r[1]).ToList();
+
+                if (toCheck.Count > 0)
+                {
+                    ranges.Add([test[0], toCheck.Last()[1]]);
+                    ranges.Remove(test);
+                    toCheck.ForEach(r => ranges.Remove(r));
+                    CleanRanges();
+                    break;
+                }
+
+            }
+
+            return;
+        }
+
+        Console.WriteLine(ranges.Sum(r => r[1] - r[0] + 1));
+    }
+}
